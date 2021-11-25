ab993fce0edc842b5a1e98bd40262ccf3fc8853c
Michael Chinye <michaelchinyechinye2018@gmail.com>
2021-11-25 17:46:48 +0100

    Dynamically populated the movie from firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,37 @@
 import styled from "styled-components";
+import { useState,useEffect } from "react";
 import ImageSlider from "./ImageSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
 function Home(){
+     const [isLoading, setIsLoading] = useState(true)
+    const [loadedMovies, setloadedMovies] = useState([]);
+    useEffect(() => {
+        setIsLoading(true)
+        fetch('https://disney-clone-99f2f-default-rtdb.firebaseio.com/Movies.json').then(response => {
+            return response.json();
+        }).then(data => {
+
+          const moviesArray = data;
+          setIsLoading(false)
+          setloadedMovies(moviesArray);
+          
+        });
+    }, []);
+   
+    if(isLoading){
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        )
+    }
+     
      return(
           <Container> 
              <ImageSlider/>
              <Viewers/>
-             <Movies/>
+             <Movies movies = {loadedMovies} />
           </Container>
      );
 }
