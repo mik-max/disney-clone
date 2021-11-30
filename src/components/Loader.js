17802888cff1dd3de92ee11e87d67459b0867583
Michael Chinye <michaelchinyechinye2018@gmail.com>
2021-11-30 11:46:02 +0100

    Adjusted loader to look well on mobile and updated the firebase.json

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -12,10 +12,10 @@ export default Loader
 
 const Spiner = styled.div`
      border: 16px solid #f3f3f3; /* Light grey */
-     border-top: 16px solid #090b13; /* Blue */
+     border-top: 16px solid #090b13; /* dark */
      border-radius: 50%;
-     width: 120px;
-     height: 120px;
+     width: 100px;
+     height: 100px;
      animation: spin 2s linear infinite;
      position: absolute;
      top: 50%;
@@ -25,5 +25,15 @@ const Spiner = styled.div`
      @keyframes spin {
      0% { transform: rotate(0deg); }
      100% { transform: rotate(360deg); }
-}
+     
+     }
+
+     @media(max-width: 480px){
+          width: 100px;
+          height: 100px;
+          position: absolute;
+          top: 50%;
+          left: 40%;
+          
+     }
 `
